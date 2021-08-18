74c97e20c2202bcbbd1e80a35db6167a309ab4e9
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2021-08-18 09:28:54 -0400

    fix: scheduling dance instead of workout

--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
 import puppeteer, { Page } from "puppeteer";
-import userAgents from "user-agents"
-import yargs from 'yargs';
+import userAgents from "user-agents";
+import yargs from "yargs";
 
 async function logarNaSmart(pagina: Page, cpf: string, senha: string) {
   await pagina.goto("https://www.smartfit.com.br");
 
-  (await pagina.$x('//a[text()="Agende seu horário"]'))[0]!.click()
-  await pagina.waitForNavigation()
+  (await pagina.$x('//a[text()="Agende seu horário"]'))[0]!.click();
+  await pagina.waitForNavigation();
 
   await pagina.type("#fitness_classes_session_login", cpf);
   await pagina.type("#fitness_classes_session_password", senha);
   await pagina.click(".fitness-classes__body-container__form__submit");
 
-  await pagina.waitForSelector(".Message__item")
+  await pagina.waitForSelector(".Message__item");
 }
 
 async function marcarHora(pagina: Page, dataHora: string) {
-  const elemento = await pagina.$x(`//div[contains(text(), "${dataHora}")]/parent::div/following-sibling::div[2]/a`)
-  await elemento[0].click()
-  await pagina.waitForNavigation()
+  const elemento = await pagina.$x(
+    `//div[contains(text(), "${dataHora}")]/parent::div//parent::div[contains(@class, "smart")]/div/a[contains(@class, "Button")]`
+  );
+  await elemento[0].click();
+  await pagina.waitForNavigation();
 
   await pagina.screenshot({
     path: "marcado.png",
@@ -27,16 +29,19 @@ async function marcarHora(pagina: Page, dataHora: string) {
 
 async function main() {
   const args = await yargs.options({
-    'cpf': { type: 'string', demandOption: true },
-    'senha': { type: 'string', demandOption: true },
-    'horario': { type: 'string', demandOption: true },
+    cpf: { type: "string", demandOption: true },
+    senha: { type: "string", demandOption: true },
+    horario: { type: "string", demandOption: true },
   }).argv;
 
-  console.log(args)
+  console.log(args);
 
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
   const pagina = await browser.newPage();
-  pagina.setUserAgent(userAgents.toString())
+  pagina.setUserAgent(userAgents.toString());
 
   await logarNaSmart(pagina, args["cpf"], args["senha"]);
 
@@ -44,7 +49,7 @@ async function main() {
     path: "logado.png",
   });
 
-  await marcarHora(pagina, args["horario"])
+  await marcarHora(pagina, args["horario"]);
 
   await browser.close();
 }
